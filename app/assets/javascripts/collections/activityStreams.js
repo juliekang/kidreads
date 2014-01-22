24b2df18568f96b3d702cd9ca5bccd7c4fb72ba1
Julie Kang <julie.kang@stanfordalumni.org>
2014-01-22 14:58:11 -0800

    Rearranging buttons

--- app/assets/javascripts/collections/activityStreams.js
+++ app/assets/javascripts/collections/activityStreams.js
@@ -1,4 +1,10 @@
 KR.Collections.ActivityStreams = Backbone.Collection.extend({
   url: "/kr/streams",
-  model: KR.Models.ActivityStreamItem
+  model: KR.Models.ActivityStreamItem,
+
+  // parse: function(response) {
+  //   this.page_number = parseInt(response.page);
+  //   this.total_pages = parseInt(response.total_pages);
+  //   return response.models;
+  // }
 });
